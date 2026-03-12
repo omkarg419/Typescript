5a9e6bca38a7530daeb0577baf2ec4eb0f7ea55f
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-03-12 19:46:50 +0530

    feat: implement update functionality for Todo items with validation

--- InMemoryDB/src/app/todo/controller.ts
+++ InMemoryDB/src/app/todo/controller.ts
@@ -34,6 +34,28 @@ class TodoController {
 			return res.status(500).json({ error: "validation failed" });
 		}
 	}
+
+	public async handleUpdateTodo(req: Request, res: Response) {
+		const { id } = req.params;
+
+		const validateBody = await todoValidationSchema.parseAsync(req.body);
+		if (!validateBody) {
+			return res.status(404).json({ error: "invalid credentials" });
+		}
+
+		const { title, description, isCompleted } = req.body;
+
+		const toUpdateTodo = this._db.find((e) => e.id === id);
+
+		if (!toUpdateTodo) {
+			return res.status(404).json({ massage: "todo not found" });
+		}
+		toUpdateTodo.title = title;
+		toUpdateTodo.description = description;
+		toUpdateTodo.isCompleted = isCompleted;
+
+		return res.status(201).json({ UpdatedTodo: toUpdateTodo });
+	}
 }
 
 export default TodoController;
